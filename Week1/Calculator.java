42fc6d2eba10790d25d495879957164ed3b8bda4
dlcodb94 <109951560+dlcodb94@users.noreply.github.com>
2023-09-19 01:35:49 +0900

    Week1 과제 제출

---
+++ Week1/Calculator.java
@@ -0,0 +1,9 @@
+public interface Calculator {
+    int plus(int a, int b); // a + b 값 반환
+
+    int minus(int a, int b); // a - b 값 반환
+
+    int mul(int a, int b); // a * b 값 반환
+
+    int div(int a, int b); // a / b 값 반환
+}
